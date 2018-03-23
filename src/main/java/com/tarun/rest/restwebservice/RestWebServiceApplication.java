077235569cde2b16b5729db226540e9888ca22c2
tgarg6343 <tgarg6343@gmail.com>
2018-03-23 18:26:20 +0530

    swagger added

--- src/main/java/com/tarun/rest/restwebservice/RestWebServiceApplication.java
+++ src/main/java/com/tarun/rest/restwebservice/RestWebServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
@@ -17,15 +18,15 @@ public class RestWebServiceApplication {
 	}
 	@Bean
 	public LocaleResolver localeResolver() {
-		SessionLocaleResolver localeResolver=new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver=new AcceptHeaderLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
 	}
-	@Bean
+	/*@Bean
 	public ResourceBundleMessageSource bundleMessageSource() {
 		ResourceBundleMessageSource messageSource= new ResourceBundleMessageSource();
 		messageSource.setBasename("messages");
 		return messageSource;
-	}
+	}*/
 	
 }
